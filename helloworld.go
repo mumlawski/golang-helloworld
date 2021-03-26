34a60643664bb1539c06e1fdb840869b1925fbed
Michal Umlawski <mumlawski@gmail.com>
2021-03-26 15:21:24 +0100

    initial commit

---
+++ helloworld.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("helloworld: received a request")
+	response := os.Getenv("RESPONSE")
+	if response == "" {
+		response = "Hello World"
+	}
+	fmt.Fprintf(w, response)
+}
+
+func main() {
+	log.Print("helloworld is starting...")
+
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("helloworld: listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
